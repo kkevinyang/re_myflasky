16bb7b316fc63a2238e53f858f512b30a7b9878b
kkevinyang <sygtxyj@gmail.com>
2016-08-12 22:46:16 +0800

    user frofile

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -2,6 +2,7 @@ import unittest
 from app import create_app, db
 from app.models import User, AnonymousUser, Role, Permission
 import time
+from datetime import datetime
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -17,6 +18,8 @@ class UserModelTestCase(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
+# 身份和权限检测
+
     def test_roles_and_permissions(self):
         u = User(email='john@example.com', password='cat')
         self.assertTrue(u.can(Permission.WRITE_ARTICLES))
@@ -25,6 +28,26 @@ class UserModelTestCase(unittest.TestCase):
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+
+# 时间戳和资料检测
+
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
 
     def test_password_setter(self):
         u = User(password='cat')
