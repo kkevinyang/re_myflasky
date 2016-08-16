65c03a2b00c613712dabc0b03ec773b948a80a52
kkevinyang <sygtxyj@gmail.com>
2016-08-16 14:25:11 +0800

    frofile editor

--- app/decorators.py
+++ app/decorators.py
@@ -3,10 +3,12 @@ from flask import abort
 from flask_login import current_user
 from .models import Permission
 
+#检查用户权限
+
 
 def permission_required(permission):
     def decorator(f):
-        @wraps(f)
+        @wraps(f)  # 消除装饰的副作用，能保留原有函数的名称和docstring
         def decorated_function(*args, **kwargs):
             if not current_user.can(permission):
                 abort(403)
