65dacba52f8805a511b1c0301a84c70c4db060e6
kkevinyang <sygtxyj@gmail.com>
2016-08-31 09:28:50 +0800

    add utf-8 to everyone

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, Regexp
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 from flask import render_template, session, redirect, url_for, current_app, abort, flash, request
 from .. import db
 from ..models import User, Role, Permission, Post, Comment
